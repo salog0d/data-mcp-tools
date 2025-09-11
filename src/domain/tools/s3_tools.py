e1972d35f61db919a9c8b2509782b8b485fb4c38
Salog0d <david.salomon.nava11@gmail.com>
2025-09-11 14:02:37 -0600

    feat(setup): basic setup for s3 functions opperations

---
+++ src/domain/tools/s3_tools.py
@@ -0,0 +1,21 @@
+from schemas import S3ListRequest, S3ListResponse
+from domain.s3_client import S3Client as client
+ 
+class S3Tools:
+    
+    @staticmethod
+    def list_objects(request: S3ListRequest) -> S3ListResponse:
+        pass
+
+    def upload_object():
+        pass
+
+    def download_object():
+        pass
+
+    def delete_object():
+        pass
+    
+    def share_object():
+        pass
+
